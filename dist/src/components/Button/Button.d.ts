e5420896305c28c4a6725dba9c51c4739b572b49
chocoscoding <chocoscoding@gmail.com>
2023-07-14 14:42:09 +0100

    refactor(📦 removed dist ): removed dist from git directory
    Dist is only required for npm and not needed on git. Could be added later on if need be.
    
    BREAKING CHANGE: Used dist only in working directory and not on git

--- dist/src/components/Button/Button.d.ts
+++
@@ -1,2 +0,0 @@
-import type { ButtonProps } from './Button.types';
-export declare const Button: ({ primary, size, backgroundColor, label, color, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
